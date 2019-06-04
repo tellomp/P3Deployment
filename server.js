97022d564801db3610988c8159f8503f76424caa
Pia Tello <piatello@Pias-MacBook-Pro.local>
2019-06-04 03:56:52 -0400

    path

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
+const path = require("path");
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
@@ -17,6 +18,17 @@ app.use(routes);
 
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/dangerousintersectionslist");
+
+
+
+// ... other app.use middleware 
+app.use(express.static(path.join(__dirname, "client", "build")))
+
+// ...
+// Right before your app.listen(), add this:
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
 // Start the API server
 app.listen(PORT, function() {
